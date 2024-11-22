9ce2ca7f2367b42b8094ee5f577a7314f4dc6703
sssan-tech <sasaki.re2019@gmail.com>
2024-11-22 14:15:16 +0900

    Itemsテーブルのカラム設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_20_054141) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_22_023230) do
+  create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_duration_id", null: false
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_items_on_users_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "last_name", null: false
     t.string "first_name", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_20_054141) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users", column: "users_id"
 end
