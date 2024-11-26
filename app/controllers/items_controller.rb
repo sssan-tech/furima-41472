5721cb082ceda967d261f95db3ef99efd9a784d7
sssan-tech <sasaki.re2019@gmail.com>
2024-11-26 17:07:46 +0900

    permit内引数:userの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :user, :image, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_duration_id).merge(user_id: current_user.id) # rubocop:disable Layout/LineLength
+    params.require(:item).permit(:name, :description, :price, :image, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_duration_id).merge(user_id: current_user.id) # rubocop:disable Layout/LineLength
   end
 
   def move_to_index
