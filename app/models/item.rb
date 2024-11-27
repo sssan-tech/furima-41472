0cb91a99d5461d83d7282fd8176c952e2eb93224
sssan-tech <sasaki.re2019@gmail.com>
2024-11-27 16:13:25 +0900

    Rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_duration
   has_one_attached :image
-  
+
   with_options presence: true do
     validates :name, :description, :user, :image
   end
 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true } # rubocop:disable Layout/LineLength
 
-  with_options numericality: { other_than: 0 , message: "can't be blank" } do
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
     validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_duration_id
   end
 end
