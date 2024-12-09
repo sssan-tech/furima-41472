a809c6416f14d15eb8f72bf99841ec595f36a2f9
sssan-tech <sasaki.re2019@gmail.com>
2024-12-09 18:22:43 +0900

    ユーザーステータス毎の購入画面への遷移可否設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
   before_action :set_item
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_index, only: :index
 
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
@@ -22,6 +24,10 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id != @item.user.id && @item.order.blank?
   end
 
   def order_params
